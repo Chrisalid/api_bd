db6c1db17b94b64a78fb68832f170570d0c18977
Chrisopher <christopherfeitosa17@hotmail.com>
2021-07-19 23:28:19 -0300

    Add models. py utils.py & updated .gitignore

---
+++ models.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///activities.db', convert_unicode=True)
+
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+class Persons(Base):
+    __tablename__ = 'persons'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), index=True)
+    age = Column(Integer)
+
+
+    def __repr__(self):
+        return f'<Person {self.name}>'
+
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+
+class Activities(Base):
+    __tablename__ = 'activities'
+    id = Column(Integer, primary_key=True)
+    activity = Column(String(80))
+    person_id = Column(Integer, ForeignKey('persons.id'))
+    person = relationship('Persons')
+
+
+    def __repr__(self):
+        return f'<Activities {self.activity}>'
+
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == '__main__':
+    init_db()
---
+++ utils.py
@@ -0,0 +1,58 @@
+from models import Persons, Activities, db_session
+
+
+def insert_person():
+    person = Persons(name='Glauber', age=22)
+    print(person)
+    person.save()
+
+
+def update_person():
+    person = Persons.query.filter_by(name='Chris').first()
+    person.age = 21
+    person.save()
+
+
+def delete_person():
+    person = Persons.query.filter_by(name='Glauber').first()
+    person.delete()
+
+
+def query_persons():
+    person = Persons.query.all()
+    print(person)
+    person = Persons.query.filter_by(name='Glauber').first()
+    print(person.id)
+
+
+def insert_activity():
+    activity = Activities(activity='Front-End', person_id= 1)
+    print(activity)
+    activity.save()
+
+
+def update_activity():
+    activity = Activities.query.filter_by().first()
+    activity.save()
+
+
+def delete_activity():
+    activity = Activities.query.filter_by().first()
+    activity.delete()
+
+
+def query_activity():
+    # activity = Activities.query.all()
+    # print(activity)
+    activity = Activities.query.filter_by(activity='Front-End').first()
+    print(activity)
+
+
+if __name__ == '__main__':
+    # insert_person()
+    # update_person()
+    # delete_person()
+    # query_persons()
+    # insert_activity()
+    # query_activity()
+    pass
